eb7c2ed00d4bb42224085a540a644424f0508b51
Leopold Luley <git@leopoldluley.de>
2020-04-12 14:18:47 +0200

    Add the lens that has to be generated

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,21 @@ fn build_ui() -> impl Widget<AppState> {
         .with_spacer(20.0)
         .with_child(
             match_widget! { Event,
-                Event::Click(u32, u32) => Label::new("click"),
+                Event::Click(u32, u32) => Label::dynamic(
+                    |data: &(u32, u32), _| format!("Click at x={}, y={}", data.0, data.1)
+                ).lens(druid::lens::Map::new(
+                    |d: &Event| match d {
+                        Event::Click(a0, a1) => (a0.clone(), a1.clone()),
+                        _ => unreachable!(),
+                    },
+                    |d: &mut Event, new: (u32, u32)| match d {
+                        Event::Click(a0, a1) => {
+                            *a0 = new.0;
+                            *a1 = new.1;
+                        },
+                        _ => unreachable!(),
+                    }
+                )),
                 Event::Key(char) => Button::new("key")
                     .on_click(|_, _, _| println!("Key was clicked")),
                 Event::Unknown => SizedBox::empty(),
